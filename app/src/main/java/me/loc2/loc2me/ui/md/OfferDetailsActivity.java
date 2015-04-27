87047d76a2b30f0628aecadc8b3748ec16902a05
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2015-04-27 23:13:53 +0300

    Orangi!

--- app/src/main/java/me/loc2/loc2me/ui/md/OfferDetailsActivity.java
+++ app/src/main/java/me/loc2/loc2me/ui/md/OfferDetailsActivity.java
@@ -1,12 +1,16 @@
 package me.loc2.loc2me.ui.md;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.transition.Transition;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,24 +22,119 @@ import me.loc2.loc2me.R;
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class OfferDetailsActivity extends ActionBarActivity {
 
+    private static final long ANIM_DURATION = 1000;
+    private View bgViewGroup;
+
     public static final String OFFER = "OFFER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         OfferStub offer = (OfferStub)getIntent().getParcelableExtra(OFFER);
-        OfferDetailedFragment offerDetailedFragment;
-        if (savedInstanceState == null) {
-            offerDetailedFragment = new OfferDetailedFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, offerDetailedFragment)
-                    .commit();
-        }
-        else {
-            offerDetailedFragment = (OfferDetailedFragment)getSupportFragmentManager().findFragmentById(R.id.container);
-        }
-        offerDetailedFragment.setOffer(offer);
+//        OfferDetailedFragment offerDetailedFragment;
+//        if (savedInstanceState == null) {
+//            offerDetailedFragment = new OfferDetailedFragment();
+//            getSupportFragmentManager().beginTransaction()
+//                    .add(R.id.container, offerDetailedFragment)
+//                    .commit();
+//        }
+//        else {
+//            offerDetailedFragment = (OfferDetailedFragment)getSupportFragmentManager().findFragmentById(R.id.container);
+//        }
+//        offerDetailedFragment.setOffer(offer);
         setContentView(R.layout.offer_details);
+        setUpLayout();
+        setUpWindowAnimations();
+    }
+
+    private void setUpLayout() {
+        bgViewGroup = findViewById(R.id.backgroundViewGroup);
+    }
+
+    private void setUpWindowAnimations() {
+        setupEnterAnimations();
+        setupExitAnimations();
+    }
+
+    private void setupEnterAnimations() {
+        Transition enterTransition = getWindow().getSharedElementEnterTransition();
+        enterTransition.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {}
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                animateRevealShow(bgViewGroup);
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {}
+
+            @Override
+            public void onTransitionPause(Transition transition) {}
+
+            @Override
+            public void onTransitionResume(Transition transition) {}
+        });
+    }
+
+    private void setupExitAnimations() {
+        Transition sharedElementReturnTransition = getWindow().getSharedElementReturnTransition();
+        sharedElementReturnTransition.setStartDelay(ANIM_DURATION);
+
+
+        Transition returnTransition = getWindow().getReturnTransition();
+        returnTransition.setDuration(ANIM_DURATION);
+        returnTransition.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+                animateRevealHide(bgViewGroup);
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+            }
+        });
+    }
+
+    private void animateRevealShow(View viewRoot) {
+        int cx = (viewRoot.getLeft() + viewRoot.getRight()) / 2;
+        int cy = (viewRoot.getTop() + viewRoot.getBottom()) / 2;
+        int finalRadius = Math.max(viewRoot.getWidth(), viewRoot.getHeight());
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(viewRoot, cx, cy, 0, finalRadius);
+        viewRoot.setVisibility(View.VISIBLE);
+        anim.setDuration(ANIM_DURATION);
+        anim.start();
+    }
+
+    private void animateRevealHide(final View viewRoot) {
+        int cx = (viewRoot.getLeft() + viewRoot.getRight()) / 2;
+        int cy = (viewRoot.getTop() + viewRoot.getBottom()) / 2;
+        int initialRadius = viewRoot.getWidth();
+
+        Animator anim = ViewAnimationUtils.createCircularReveal(viewRoot, cx, cy, initialRadius, 0);
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                viewRoot.setVisibility(View.INVISIBLE);
+            }
+        });
+        anim.setDuration(ANIM_DURATION);
+        anim.start();
     }
 
     /**
